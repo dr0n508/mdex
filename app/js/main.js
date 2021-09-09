251964e4407a0add9fdb12a11297cd2ddbdb1b7c
dr0n <kornienko.andriy@gmail.com>
2021-09-09 16:48:45 +0300

    start commit

--- app/js/main.js
+++ app/js/main.js
@@ -35,4 +35,53 @@ $(document).ready(function() {
         });
     }
 
+    //tooltips
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+
+    //countdown
+    function makeTimer() {
+
+        //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");
+        var endTime = new Date("29 October 2021 9:56:00 GMT+01:00");
+        endTime = (Date.parse(endTime) / 1000);
+
+        var now = new Date();
+        now = (Date.parse(now) / 1000);
+
+        var timeLeft = endTime - now;
+
+        var days = Math.floor(timeLeft / 86400);
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+        if (hours < "10") { hours = "0" + hours; }
+        if (minutes < "10") { minutes = "0" + minutes; }
+        if (seconds < "10") { seconds = "0" + seconds; }
+
+        $("#days").html(days);
+        $("#hours").html(hours);
+        $("#minutes").html(minutes);
+        $("#seconds").html(seconds);
+
+    }
+
+    setInterval(function() { makeTimer(); }, 1000);
+
+
+
+
+});
+
+
+$(document).ready(function () {
+
+    $('#dtBasicExample').DataTable({
+        "paging": false // false to disable pagination (or any other option)
+    });
+    $('.dataTables_length').addClass('bs-select');
 });
